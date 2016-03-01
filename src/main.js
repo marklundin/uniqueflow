5911ea087194353dba7a828f9573e74bf925678b
Mark Lundin <mark@mark-lundin.com>
2016-03-02 00:45:21 +0100

    new dpi settings

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ let InstallationArtwork = ( canvas, filePath, dpi ) => {
 
 	 // Style it
 	let artwork = new Artwork( canvas, filePath, {
-		timeScale: 0.83, 
+		timeScale: 0.9, 
 		useTextureNoise:false, 
 		pixelRatio })
 
